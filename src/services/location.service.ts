1ef40c9da5edf5372ee4ee40da89a8c1974fd523
Tiago de Oliveira <tiago.zdo@gmail.com>
2025-01-08 19:51:46 -0300

    refactor the index and show method in LocationService

--- src/services/location.service.ts
+++ src/services/location.service.ts
@@ -8,39 +8,40 @@ interface LocationType {
 class LocationService {
 
     async index(page: number, limit: number) {
-        // try {
-        //     const [data, total] = await Location.findAndCount({
-        //         relations: ["reformersBornHere", "reformersDiedHere"],
-        //         skip: (page - 1) * limit,
-        //         take: limit,
-        //     });
+        try {
+            const [data, total] = await Location.findAndCount({
+                relations: ["birthReformers", "deathReformers"],
+                skip: (page - 1) * limit,
+                take: limit,
+            });
 
-        //     const pages = Math.ceil(total / limit);
+            const pages = Math.ceil(total / limit);
 
-        //     const results = data.map(location => ({
-        //         id: location.id,
-        //         name: location.name,
-        //         reformersBornHere: [
-        //             ...location.reformersBornHere.map(reformer => `http://localhost:3001/api/reformer/${reformer.id}`),
-        //         ],
-        //         reformersDiedHere: [
-        //             ...location.reformersDiedHere.map(reformer => `http://localhost:3001/api/reformer/${reformer.id}`),
-        //         ],
-        //         created: location.createdAt.toISOString(),
-        //     }));
+            const results = data.map(location => ({
+                id: location.id,
+                name: location.name,
+                reformersBornHere: [
+                    ...location.birthReformers.map(reformer => `http://localhost:3001/api/reformer/${reformer.id}`),
+                ],
+                reformersDiedHere: [
+                    ...location.deathReformers.map(reformer => `http://localhost:3001/api/reformer/${reformer.id}`),
+                ],
+                created: location.createdAt.toISOString(),
+            }));
 
-        //     return {
-        //         info: {
-        //             count: total, pages,
-        //             next: page < pages ? `http://localhost:3001/api/location?page=${page + 1}` : null,
-        //             prev: page > 1 ? `http://localhost:3001/api/location?page=${page - 1}` : null,
-        //         },
-        //         results,
-        //     };
+            return {
+                info: {
+                    count: total,
+                    pages,
+                    next: page < pages ? `http://localhost:3001/api/location?page=${page + 1}` : null,
+                    prev: page > 1 ? `http://localhost:3001/api/location?page=${page - 1}` : null,
+                },
+                results,
+            };
 
-        // } catch (error) {
-        //     throw new Error("Error retrieving locations from the database");
-        // }
+        } catch (error) {
+            throw new Error("Error retrieving locations from the database");
+        }
     }
 
     async store(location: LocationType) {
@@ -56,28 +57,28 @@ class LocationService {
     }
 
     async show(ids: number[]) {
-        // try {
-        //     const data = await Location.find({
-        //         where: ids.map(id => ({ id })),
-        //         relations: ["reformersBornHere", "reformersDiedHere"],
-        //     });
+        try {
+            const data = await Location.find({
+                where: ids.map(id => ({ id })),
+                relations: ["birthReformers", "deathReformers"],
+            });
 
-        //     if (data.length === 0) {
-        //         throw new Error(`Location with ID(s) ${ids.join(", ")} not found.`);
-        //     }
+            if (data.length === 0) {
+                throw new Error(`Location(s) with ID(s) ${ids.join(", ")} not found.`);
+            }
 
-        //     return data.map(location => ({
-        //         id: location.id,
-        //         name: location.name,
-        //         reformersBornHere: location.reformersBornHere.map(reformer => `http://localhost:3001/api/reformer/${reformer.id}`),
-        //         reformersDiedHere: location.reformersDiedHere.map(reformer => `http://localhost:3001/api/reformer/${reformer.id}`),
-        //         created: location.createdAt.toISOString(),
-        //     }));
-
-        // } catch (error) {
-        //     throw new Error(`Error retrieving location(s) with ID(s) ${ids.join(", ")} from database`);
-        // }
+            return data.map(location => ({
+                id: location.id,
+                name: location.name,
+                reformersBornHere: location.birthReformers.map(reformer => `http://localhost:3001/api/reformer/${reformer.id}`),
+                reformersDiedHere: location.deathReformers.map(reformer => `http://localhost:3001/api/reformer/${reformer.id}`),
+                created: location.createdAt.toISOString(),
+            }));
+        } catch (error) {
+            throw new Error(`Error retrieving location(s) with ID(s) ${ids.join(", ")}`);
+        }
     }
+
 
     async update(id: number, body: LocationType) {
         // try {
